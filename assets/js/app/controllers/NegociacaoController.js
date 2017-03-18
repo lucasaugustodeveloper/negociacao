bfa8870aee09a99cce8949d6b57fefd85ea9001f
Lucas Augusto <lucas.augusto5061@gmail.com>
2017-03-18 14:22:23 -0300

    criado classe para trabalha com data e feito regra de negócio para inserir uma negociação através do método adiciona do negociação controller, começado a trabalha com o template de visualização para exibir os dados na tela

--- assets/js/app/controllers/NegociacaoController.js
+++ assets/js/app/controllers/NegociacaoController.js
@@ -6,20 +6,27 @@ class NegociacaoController {
     this._inputData = $('#data')
     this._inputQuantidade = $('#quantidade')
     this._inputValor = $('#valor')
+    this._listaNegociacoes = new ListaNegociacoes()
+    this._negociacoesView = new NegociacoesView
   }
 
   adiciona (event) {
     event.preventDefault()
+    this._listaNegociacoes.adiciona(this._criarNegociacao())
+    this._limpaFormulario()
+  }
 
-    let data = new Date(
-      ...this._inputData.value
-        .split('-')
-        .map((item, indice) => item - indice % 2)
-    )
-    let negociacao = new NegociacaoController(
-      data,
-      this._inputQuantidade,
-      this._inputValor
-    )
+  _criarNegociacao () {
+    return new Negociacao(
+      DateHelper.textoParaData(this._inputData.value),
+      this._inputQuantidade.value,
+      this._inputValor.value)
+  }
+
+  _limpaFormulario () {
+    this._inputData.value = ''
+    this._inputQuantidade.value = 1
+    this._inputValor.value = 0.0
+    this._inputData.focus()
   }
 }
